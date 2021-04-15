44e0dcb953d9d8ac434ceb7a1a8666f8ae120571
PauloAlexandre61 <paullo-alex@outlook.com>
2021-04-14 21:06:30 -0300

    Primeiro commit

--- script_criacao.R
+++ script_criacao.R
@@ -1,1 +1,8 @@
 #usethis::create_project("C:\\projetosr\\lab_ciencia_dados")
+#usethis::create_project("C:\\projetosr\\lab_ciencia_dados")
+
+#criar a estrutura git para esse projeto
+usethis::use_git()
+usethis::use_github()
+usethis::create_github_token()
+gitcreds::gitcreds_set(url = "http://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")
